7acaa4e3664dcc0783268b05e685bbdc6d802b11
Arjun Ruhela <arjunruhela05@gmail.com>
2026-09-10 02:36:33 +0530

    Delete .commands directory

--- .commands/frontend.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-title SAHAYAK frontend (Vite)
-call "C:\Users\divya\anaconda3\condabin\conda.bat" activate sahayak
-cd /d "D:\CODES\sahayak2\SAHAYAK\SAHAYAK-main\frontend"
-call npm run dev -- --port 5173 --strictPort
-echo.
-echo Frontend stopped. Press any key to close.
-pause >nul
